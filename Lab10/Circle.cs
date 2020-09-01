3bbc2a5b47c9d69d5ef3259a131e4b430157394e
ernest2358 <ej2358@yahoo.com>
2020-08-31 23:09:03 -0400

    morepractice

---
+++ Lab10/Circle.cs
@@ -0,0 +1,14 @@
+﻿namespace Lab10
+{
+    public class Circle
+    {
+        public double Bircle;
+
+        public double GetWasted()
+        {
+            var waste = 1;
+            return waste;
+        }
+
+    }
+}
---
+++ Lab10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello User, would you like to use our Circle Tester application? Y/N?");
+            var userResponse = Console.ReadLine();
+            if (userResponse == "y" || userResponse == "Y")
+            {
+                Console.WriteLine("Please enter the radius of your cirle. (Must be number)");
+                var radius = double.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("Would you like to continue? Y/N");
+                userResponse = Console.ReadLine();
+                var timesDone = 
+            }
+            Console.WriteLine($"Goodbye. You created {timesDone} ircle objects");
+            var duh = new Circle();
+        }
+    }
+}
